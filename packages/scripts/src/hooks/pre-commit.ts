e401654d370ff3c3702fde56e13e2b5be9157c3d
Leon Si <contact@leonzalion.com>
2022-05-09 15:40:59 -0400

    feat: pseudo-monorepo scripts

--- packages/scripts/src/hooks/pre-commit.ts
+++ packages/scripts/src/hooks/pre-commit.ts
@@ -1,3 +1,20 @@
-import { preCommit } from 'lion-system';
+import { execa } from 'execa';
+import { getProjectDir, preCommit } from 'lion-system';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 preCommit();
+
+// Add the changed files in all packages: https://stackoverflow.com/a/33620540
+
+const monorepoDir = getProjectDir(import.meta.url, { monorepoRoot: true });
+const packagesDir = path.join(monorepoDir, 'packages');
+
+const packageNames = fs.readdirSync(packagesDir);
+
+await Promise.allSettled(
+	packageNames.map(async (packageName) => {
+		const packageDir = path.join(packagesDir, packageName);
+		await execa('git', ['add', '.'], { cwd: packageDir });
+	})
+);
--- packages/scripts/src/hooks/pre-push.ts
+++ packages/scripts/src/hooks/pre-push.ts
@@ -1,3 +1,20 @@
-import { prePush } from 'lion-system';
+import { execa } from 'execa';
+import { getProjectDir, prePush } from 'lion-system';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 prePush();
+
+// Execute `git push` in all package directories
+
+const monorepoDir = getProjectDir(import.meta.url, { monorepoRoot: true });
+const packagesDir = path.join(monorepoDir, 'packages');
+
+const packageNames = fs.readdirSync(packagesDir);
+
+await Promise.allSettled(
+	packageNames.map(async (packageName) => {
+		const packageDir = path.join(packagesDir, packageName);
+		await execa('git', ['push'], { cwd: packageDir });
+	})
+);
